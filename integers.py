00a4a12e9e7c1f782a2974e8f36685c31e76efae
Kirill <kiryhaaa13@gmail.com>
2017-08-28 21:50:01 +0300

    Solved task with triangle

--- integers.py
+++ integers.py
@@ -1,2 +1,49 @@
 #Hello Kiryha
 #At that file you will se some basic code
+#1 task
+a = 10
+P = 4 * a
+print(P)
+
+#2 task
+a = 3
+S = a*a
+print(S)
+
+#3 task
+a = 2
+b = 1
+S = a * b
+P = 2*(a + b)
+print(S,P)
+
+#4 task
+d = 13
+Pi = 3.14
+L = Pi * d
+print(L)
+
+#5 task
+a = 4
+V = a ** 3
+S = 6 * (a ** 2)
+print(V, S)
+
+#6 task
+
+
+
+
+# At that part examples from tasks.txt
+#Define triangle. And decide exist it or not?
+a = 3
+b = 7
+c = 9
+def triangle (a,b,c):
+    if (a < b + c) or (b < c + a) or (c < a + b):
+        return true
+    else:
+        return false
+#or change return for "Print (Triangle exist) etc"
+
+
