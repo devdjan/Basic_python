ad02426e2273715d92943fb0225a6e39ad17cc18
Kirill <kiryhaaa13@gmail.com>
2017-08-31 22:44:15 +0300

    Discriminant

--- integers.py
+++ integers.py
@@ -30,6 +30,18 @@ S = 6 * (a ** 2)
 print(V, S)
 
 #6 task
+import math
+import sys
+import stdio
+
+b = float(sys.argv[1])
+c = float(sys.argv[2])
+
+discriminant = b*b - 4.0*c
+d = math.sqrt(discriminant)
+stdio.writeln((-b + d) / 2.0)
+stdio.writeln((-b - d) / 2.0)
+
 
 
 
