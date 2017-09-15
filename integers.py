3d352b8868a1a9d3be55a16a5a09db7dfd8f89f5
Kirill <kiryhaaa13@gmail.com>
2017-09-15 22:46:44 +0300

    modified integers.py

--- integers.py
+++ integers.py
@@ -1,3 +1,4 @@
+# 1
 #n школьников делят k яблок поровну, неделящийся остаток остается в корзинке. Сколько яблок достанется каждому школьнику? Сколько яблок останется в корзинке?
 #Программа получает на вход числа n и k и должна вывести искомое количество яблок (два числа).
 n = int(input()) #14
@@ -7,12 +8,23 @@ a = k // n
 b = k - (a * n)
 print(a)
 print(b)
-
-
-#Maybe good idea write it using print
+#Походу, чтобы отрефакторить можно использовать "Return"
+# return(k // n)
+# return(k - (a * n))
+# or, good idea write it using print
+n = 10
+k = 24
 print(n // k)
 print(n % k)
 
+# 2
+# Дано число n. C начала суток прошло n минут. Определить, сколько часов и минут будут показывать электронные часы
+# в тот момент. Вывести два числа. Кол-во часов (0 - 23) и минут (0 - 59)
+# 24 - 1440
+n = 146
+hours = n // 1440
+minutes = n % 1440
+print(hours,minutes)
 
 
 #-----------------------------------------------------
@@ -21,20 +33,37 @@ print(n % k)
 
 # At that part examples from tasks.txt
 #Define triangle. And decide exist it or not?
-a = 3
-b = 7
-c = 9
 def triangle (a,b,c):
     if (a < b + c) and (b < c + a) and (c < a + b):
         print("True")
     else:
         print("False")
+triangle(4,5,6)
 #or change return for "Print (Triangle exist) etc."
 
-#Write an function wich creates and initialises matrix. Entry  - m,n,k
-#m, n height and width. K - element wich will be initialising the matrix.
 
-#Testing,  i want to know how built matrix, before start
+
+# Better way of solving this task. Algorithmi is the same:
+# Define function triangle which take
+# three parameters a,b,c. Then we does'nt print out true, false,
+# we use "Return"(true of false)
+
+# def triangle (a,b,c):
+#     return (a < b + c) and (b < c + a) and (c < a + b)
+#         return false
+
+# Testing values
+# We are checking how our program works( correctly or not),
+# with different values
+
+# list_check : {
+#     0,0,0 : False,
+#     1,2,3 : True
+# }
+#Loop for using
+# any(*variable). ()python tuples
+
+
 
 
 
