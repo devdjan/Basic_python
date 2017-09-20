27dd02a6ae947aa4631b55d647c731b6fbfa7510
Kirill <kiryhaaa13@gmail.com>
2017-09-20 23:41:55 +0300

    tasks with arrays

--- array.py
+++ array.py
@@ -6,16 +6,13 @@ a = [] #initializing empty array
 for i in range(n):
     a += [0.0] # addingone elem in the end of array
 
-
-
-
 # Some fundamental operations
-a = [1,2,3,4,5]
-total = 0.0
-for i in range(len(a)):
-    total += a[i]
-average = total / len(a)
-print(average)
+# a = [1,2,3,4,5]
+# total = 0.0
+# for i in range(len(a)):
+#     total += a
+# average = total / len(a)
+# print(average)
 
 # Перебор элементов массива не обращаясь к индексам явно.
 # Поместим имя массива после ключевого слова in в операторе for
@@ -25,5 +22,31 @@ for v in a:
 average = total / len(a)
 stdio.writeln(average)
 
-# Псевдонимы и копии массива.
-# Alias
+#----------------------------------------------------------
+#Write a Python program to create an array of 5 integers
+# and display the array items.
+# Access individual element through indexes
+# a = [1,4,5,6,7]
+# for i in range(a):
+#     print(i)
+#     print(i[-1])
+array = [1,4,5,6,7]
+for i in array:
+    print(i)
+    print(array[1]) # five times 4 4 4 4 4
+
+#----------------------------------------------------------
+#Write a Python program to append a new item to the end of the array.
+array = [1,2,3,4,5,0]
+array.append(6)
+print(array)
+
+#----------------------------------------------------------
+#Write a Python program to reverse the order of the items in the array.
+#Maybe using .reverse
+
+array = [1,2,3,6,7,8,0,-1]
+array.reverse()
+print(array)
+
+#Tomorrov i will try to write some function of "reverse"
