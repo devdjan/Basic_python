aec76d67d4a383e03fd5caa747fe81f22af8a9c8
Kirill <kiryhaaa13@gmail.com>
2017-09-29 13:37:18 +0300

    prime sieve

--- array.py
+++ array.py
@@ -138,6 +138,25 @@ if not isCollected[value]:
     isCollected[value] = True
 stdio.writeln(count)
 
+#----------------------------------------------------------
+# Решето Эратосфена
+#----------------------------------------------------------
+import stdarray
+import stdio
+
+n = 10 # Аргумент
+isPrime = stdarray.create1D(n+1, True) #
+
+for i in range(2, n):
+    if(isPrime[i]):
+        for j in range(2, n//i + 1):
+            isPrime[i * j] = False
+# Подсчет простых чисел
+count = 0
+for i in range(2, n+1):
+    if (isPrime[i]):
+        count += 1
+stdio.write(count)
 
 
 
