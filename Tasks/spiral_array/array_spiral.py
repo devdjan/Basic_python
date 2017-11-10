bc8203c21096be8737136de08b20f2325b369773
Kirill <kiryhaaa13@gmail.com>
2017-11-10 23:29:12 +0300

    Reviewing some examples of spiral algo

--- Tasks/spiral_array/array_spiral.py
+++ Tasks/spiral_array/array_spiral.py
@@ -47,3 +47,74 @@ def spiral_func(n):
 # Вызываем вышеописанну. функцию
 # Вызываем ее с аргументом, который вводит пользователь с клавиатуры
 spiral_func(int(input()))
+
+
+
+# 1
+# The next example
+# From stackoverflow
+# First step: Pop top row
+# Second step: Transpose and flip upside-down(same as
+# rotate 90 degrees counter-clockwise)
+# Go to 1 step
+# ----------------------------------------------------
+# 1. Верхнюю строчку, потом
+# 2. Транспонирум и переворачиваем матрицу вверх-вниз,
+# тоже самое что и вращение на 90 градусов против часовой стрелки
+# 3. Вернуться к шагу 1
+
+import intertools
+import i
+arr = [[1,2,3,4],
+       [12,13,14,5],
+       [11,16,15,6],
+       [10,9,8,7]]
+
+def transpose_and_yield_top(arr):
+    while arr:
+        yield arr[0]
+        arr = list(reversed(zip(*arr[1:])))
+
+print list(itertools.chain(*transpose_and_yield_top(arr)))
+
+
+# 2
+# Generating spiral matrix we can treated like a snake moving tovards
+# boundary an turning on when hitting the boundary or itself
+
+
+ar = [
+    [0, 1, 2, 3, 4],
+    [15, 16, 17, 18, 5],
+    [14, 23, 24, 19, 6],
+    [13, 22, 21, 20, 7],
+    [12, 11, 10, 9, 8]]
+
+def print_spiral(ar):
+    # if rect array
+    rows, cols = len(ar), len(ar[0])
+    r, c = 0, -1 # start here
+    nextturn = stepsx = cols #move so many steps
+    stepsy = rows - 1
+    inc_c, inc_r = 1, 0# at each step move this much
+    turns = 0 # how many times our snake had turned
+
+    for i in range(rows, cols):
+        c += inc_c
+        r += inc_r
+
+        print(ar[r][c]), # maybe row and cols
+
+        if i == nextturn - 1:
+            turns += 1
+            # at each turn reduce how many steps we go next
+            if turns % 2 == 0:
+                nextturn += stepsx
+                stepsy -= 1
+            else:
+                nextturn += stepsy
+                stepsx -= 1
+            # changing the Directions
+            inc_c, inc_r = -inc_r, inc_c
+
+print_spiral(ar)
