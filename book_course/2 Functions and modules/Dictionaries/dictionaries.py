3692f2cf8e2967cd390dffd7bac999e49dda8d1e
Kirill <kiryhaaa13@gmail.com>
2017-11-22 11:25:05 +0300

    Added new example in dictionary.py

--- book_course/2 Functions and modules/Dictionaries/dictionaries.py
+++ book_course/2 Functions and modules/Dictionaries/dictionaries.py
@@ -12,6 +12,23 @@ print('Sam' not in cars_dict) # True
 cars_dict[101] = 'New value'
 print(cars_dict[101]) # New value
 print(cars_dict.get('ar324df')) # Returns value - Sam Smith/ If not exists
-print(cars_dict.get('notexist')) # It will return None - not Error!
+print(cars_dict.get('notexist')) # It will return None - not
 
-#
+# Let's iterate our dictionary in loop
+dict = {'Kiryha' : 21, 'Sasha' : 19, 'Mikyta' : 22, 'Rob' : '25'}
+for key in dict:
+    print(key, end=' ')
+
+print("\n")
+# Russian: переберем, только явно будем исп. функцию keys - у словаря.
+for key in dict.keys():
+    print(key, end=' ')
+
+print("\n") # Next line
+for value in dict.values():
+    print(value, end=' ')
+
+print("\n") # Next line
+# Now we are printing out <key> <value> Using dictionry function .items()
+for keys, values in dict.items():
+    print(keys,values, end="; ")
