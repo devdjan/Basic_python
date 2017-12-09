6583811af561442fbecbcd81d6544b92ce88af0d
Kirill <kiryhaaa13@gmail.com>
2017-12-09 18:57:36 +0300

    start analyzing by describing the data

--- Tasks/titanic/titanic.py
+++ Tasks/titanic/titanic.py
@@ -1,1 +1,35 @@
 # Task from kaggle
+
+# data analysus and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
+
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Reseiving Data
+train_df = pd.read_csv('datasets/train.csv')
+test_df = pd.read_csv('datasets/test.csv')
+combine = [train_df, test_df]
+
+# Analyzing describing data
+print(train_df.columns.values)
+
+# So what features are catagorical ?
+# Survived, Sex, and Embarked. Passenger class
+
+#preview the data
+train_df.head()
