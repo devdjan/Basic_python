ec8e8de82c64e1c0ee951fe6acaf1756bd56aa2e
Kirill <kiryhaaa13@gmail.com>
2018-03-08 00:37:49 +0300

    Completed some tasks for exercise 2, keep going

--- Courses/Introduction to CS MITx/Week 2/exercise_2.py
+++ Courses/Introduction to CS MITx/Week 2/exercise_2.py
@@ -2,11 +2,11 @@
 x = 25
 epsilon = 0.01
 step = 0.1
-guess = 0.0
+guess = 5
 
 while guess <= x: # Пока 0.0 меньше 25
     if abs(guess**2 - x) < epsilon: # (00^2 - 25) < 0.01 Переходим к else
-        break # Выходим так как получается отриц число
+        break # Выходим так как по МОДУЛЮ число никак не меньше epsilo
     else: #
         guess += step # Увеличиваем переменную на 0.1 и так аж до 5 (4.99987 и тд.
 
@@ -23,12 +23,34 @@ print('------------------------------------')
 x1 = 25
 epsilon1 = 0.01
 step1 = 0.1
-guess1 = 0.0
+guess1 = 0.1
 
-while guess1 <= x:
-    if abs(guess1**2 - x) >= epsilon1:
-        guess += step1
+while guess1 <= x1:
+    if abs(guess1**2 - x1) >= epsilon1:
+        guess1 += step1
 
-if abs(guess**2 - x) >= epsilon1:
+if abs(guess**2 - x1) >= epsilon1:
     print('failed')
-    
+else:
+    print('succeeded: ' + str(guess1))
+
+print('------------------------------------')
+
+# ----------------------
+
+# Task 3
+x2 = 25
+epsilon2 = 0.01
+step2 = 0.1
+guess2 = 0.0
+
+while abs(guess2**2 - x2) >= epsilon2:
+    if guess2 <= x2:
+        guess2 += step2
+    else:
+        break
+
+if abs(guess2**2 - x2) >= epsilon2:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess2))
